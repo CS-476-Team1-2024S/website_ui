411592d1ec4690198ffc32402de6e42b594c8f8f
Levi Blankenheim <levyguy88@gmail.com>
2024-04-04 11:54:40 -0500

    Basic React/react-router-dom setup

---
+++ src/pages/About.js
@@ -0,0 +1,11 @@
+import React from 'react';
+const About = () => {
+    return (
+        <div>
+            <h1>About</h1>
+            <p>This is the about page</p>
+            <a href="/">Back to Home</a>
+        </div>
+    );
+};
+export default About;
---
+++ src/pages/Home.js
@@ -0,0 +1,11 @@
+import React from 'react';
+const Home = () => {
+    return (
+        <div>
+            <h1>Welcome to our knowledgebase</h1>
+            <p>This is the home page</p>
+            <a href="/about">Go to About</a>
+        </div>
+    );
+};
+export default Home;
