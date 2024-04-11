8172d1727ac9f6bf52b0cc8188e2fa94b8eecd62
Levi Blankenheim <levyguy88@gmail.com>
2024-04-11 10:15:46 -0500

    Removed breaking changes

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import './App.css';
-import { HashRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
 import About from './pages/About';
 
