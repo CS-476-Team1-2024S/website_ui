4e12e5d97bbae8c7596d8eda3a7b94a8da5b645a
Nelson Cowan <cowannb17@uww.edu>
2024-04-11 13:28:21 -0500

    Added packages, javascript, css for darkmode toggle

--- src/App.js
+++ src/App.js
@@ -2,10 +2,12 @@ import './App.css';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
 import About from './pages/About';
+import ThemeButton from './components/ThemeButton';
 
 function App() {
   return (
     <>
+      <ThemeButton></ThemeButton>
       <Router>
         <Routes>
           <Route path="/" element={<Home/>}/>
--- src/components/ThemeButton.js
+++ src/components/ThemeButton.js
@@ -1,8 +1,19 @@
 import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faMoon, faSun } from '@fortawesome/free-solid-svg-icons';
 const ThemeButton = () => {
     return (
         <div className="theme-button">
-            <input type="checkbox" class="checkbox" id="chk"/>
+            <input type="checkbox" className="theme-toggle" id="chk" onChange={() => {
+                const html = document.querySelector("html");
+                const newTheme = html.getAttribute("data-theme") === "dark" ? "light" : "dark";
+                document.querySelector("html").setAttribute("data-theme", newTheme);
+            }}/>
+            <label className="theme-inner" htmlFor="chk">
+                <FontAwesomeIcon icon={ faMoon } />
+                <FontAwesomeIcon icon={ faSun } />
+                <div className="ball"></div>
+            </label>
         </div>
     );
 }
