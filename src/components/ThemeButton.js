1d5e2df298659839e8d013877ebc3820bf93f647
cowannb17 <cowannb17@uww.edu>
2024-04-11 11:47:52 -0500

    Added Content box, darkmode button, and general tempalte js files

---
+++ src/components/ThemeButton.js
@@ -0,0 +1,9 @@
+import React from 'react';
+const ThemeButton = () => {
+    return (
+        <div className="theme-button">
+            <input type="checkbox" class="checkbox" id="chk"/>
+        </div>
+    );
+}
+export default ThemeButton;
