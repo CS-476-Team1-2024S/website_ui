8e435cbca8e94681a7b4dd5f86f9eabd58b871d5
StirmelCM <StirmelCM20@uww.edu>
2024-04-24 19:05:07 -0500

    New branch w/ sample article page

--- src/pages/Article.js
+++ src/pages/Article.js
@@ -1,28 +1,21 @@
 import React from 'react';
 import Grid from '@mui/material/Grid';
 import Box from '@mui/material/Box';
-import ArticleContent from '../components/ArticleContent'; // Assuming you have a component for displaying article content
 
 const Article = () => {
     return (
-        <div className="article-page">
-            <div className="article-title">
-                <h1>Title of the Article</h1>
-                <h3>Author Name - Date</h3>
+            <div className="content">
+                <h1>How to Play Go Fish</h1>
+                <h2>Part 1 | Starting the Game</h2>
+                <p>
+                    Pick someone to shuffle and deal the cards for the first round. Go Fish is great because it can be played with as few as 2 players and up to as many as 6 players. The dealer could be the person whose birthday is coming up next, the person who won the last game, or someone else you choose.<sup>[1]</sup>
+                </p>
+                <ul>
+                    <li>Try to take turns being the dealer so that one person doesn’t get stuck doing it every time.</li>
+                </ul>
+                <a href="/">Back to Home</a>
             </div>
-            <ArticleContent />
-            <Box sx={{ flexGrow: 1 }}>
-                <Grid container spacing={2}>
-                    <Grid item xs={12}>
-                        <div className="article-related">
-                            <h2>Related Articles</h2>
-                            {/* Render related article links or components here */}
-                        </div>
-                    </Grid>
-                </Grid>
-            </Box>
-        </div>
-    );
+        );
 };
 
 export default Article;
