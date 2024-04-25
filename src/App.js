55413acc7df2707483e520675fd08c0249cc39fc
StirmelCM <StirmelCM20@uww.edu>
2024-04-25 18:39:27 -0500

    Branch based on LB-addingUserAPI with sample article (javascript) and markdown templates.

--- src/App.js
+++ src/App.js
@@ -2,9 +2,9 @@ import './App.css';
 import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
 import About from './pages/About';
-import Article from './pages/Article';
 import ThemeButton from './components/ThemeButton';
-import Template from './pages/Template'
+import LoginPage from './pages/LoginPage';
+
 
 function App() {
   return (
@@ -14,8 +14,7 @@ function App() {
         <Routes>
           <Route path="/" element={<Home/>}/>
           <Route path="about" element={<About/>}/>
-          <Route path="Article" element={<Article/>}/>
-          <Route path="Template" element={<Template/>}/>
+          <Route path="login" element={<LoginPage/>}/>
         </Routes>
       </Router>
     </>
--- src/components/Directory.js
+++ src/components/Directory.js
@@ -2,14 +2,13 @@ import React from 'react';
 const Directory = ({ location }) => {
     return (
         <div className="sideBar">
-            <img className="logo" src="/images/logo.png" alt="Knowledgebase Site Logo"></img>
+            <img className="logo" src="images/logo.png" alt="Knowledgebase Site Logo"></img>
             <div id="directory">
                 <table>
                     <tbody>
                         <tr><td><a href="#/"><p>Home</p></a></td></tr>
                         <tr><td><a href="#about"><p>About</p></a></td></tr>
-                        <tr><td><a href="#Article"><p>Sample Article</p></a></td></tr>
-                        <tr><td><a href="#Template"><p>Markdown Test</p></a></td></tr>
+                        <tr><td><a href="#login"><p>Login</p></a></td></tr>
                     </tbody>
                 </table>
             </div>
