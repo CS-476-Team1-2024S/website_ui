0ee3a1833e4e252accdd6b92a1fefa32ef1306a5
StirmelCM <StirmelCM20@uww.edu>
2024-04-24 19:23:53 -0500

    New branch w/ sample article page

--- src/pages/Article.js
+++ src/pages/Article.js
@@ -8,7 +8,7 @@ const Article = () => {
             <ol>
                 <li>
                     <p>
-                        Pick someone to shuffle and deal the cards for the first round. Go Fish is great because it can be played with as few as 2 players and up to as many as 6 players. The dealer could be the person whose birthday is coming up next, the person who won the last game, or someone else you choose.<sup>[1]</sup>
+                        Pick someone to shuffle and deal the cards for the first round. Go Fish is great because it can be played with as few as 2 players and up to as many as 6 players. The dealer could be the person whose birthday is coming up next, the person who won the last game, or someone else you choose.
                     </p>
                     <ul>
                         <li>Try to take turns being the dealer so that one person doesn’t get stuck doing it every time.</li>
