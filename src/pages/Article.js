204254c0e35672f4e4c1170f50ba7e933f8b96e8
StirmelCM <StirmelCM20@uww.edu>
2024-04-24 19:20:41 -0500

    New branch w/ sample article page

--- src/pages/Article.js
+++ src/pages/Article.js
@@ -1,23 +1,23 @@
 import React from 'react';
-import Grid from '@mui/material/Grid';
-import Box from '@mui/material/Box';
 
 const Article = () => {
     return (
-            <div className="content">
-                <h1>How to Play Go Fish</h1>
-                <h2>Part 1 | Starting the Game</h2>
-                <ol>
+        <div className="content">
+            <h1>How to Play Go Fish</h1>
+            <h2>Part 1 | Starting the Game</h2>
+            <ul>
                 <li>
-                    Pick someone to shuffle and deal the cards for the first round. Go Fish is great because it can be played with as few as 2 players and up to as many as 6 players. The dealer could be the person whose birthday is coming up next, the person who won the last game, or someone else you choose.<sup>[1]</sup>
-                    <ul>
-                        <li>Try to take turns being the dealer so that one person doesn’t get stuck doing it every time.</li>
-                    </ul>
-                <li>
-                </ol>
-                <a href="/">Back to Home</a>
-            </div>
-        );
+                    <p>
+                        Pick someone to shuffle and deal the cards for the first round. Go Fish is great because it can be played with as few as 2 players and up to as many as 6 players. The dealer could be the person whose birthday is coming up next, the person who won the last game, or someone else you choose.<sup>[1]</sup>
+                    </p>
+                    <p>
+                        Try to take turns being the dealer so that one person doesn’t get stuck doing it every time.
+                    </p>
+                </li>
+            </ul>
+            <a href="/">Back to Home</a>
+        </div>
+    );
 };
 
 export default Article;
