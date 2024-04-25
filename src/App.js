a1bb9a6a034032498b3bc4e2cbba45f2a8caeead
StirmelCM <StirmelCM20@uww.edu>
2024-04-24 19:08:05 -0500

    New branch w/ sample article page

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import './App.css';
 import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
 import About from './pages/About';
+import Article from './pages/Article';
 import ThemeButton from './components/ThemeButton';
 
 
@@ -13,6 +14,7 @@ function App() {
         <Routes>
           <Route path="/" element={<Home/>}/>
           <Route path="about" element={<About/>}/>
+          <Route path="Article" element={<Article/>}/>
         </Routes>
       </Router>
     </>
