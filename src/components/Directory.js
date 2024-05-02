2e4e27e03beb999d20d2cfd8c68a73847daa5567
Levi Blankenheim <levyguy88@gmail.com>
2024-05-02 11:24:04 -0500

    Removed odd code

--- src/components/Directory.js
+++ src/components/Directory.js
@@ -50,20 +50,5 @@ const Directory = () => {
             </div>
         );
     }
-
-    return (
-        <div className="sideBar">
-            <img className="logo" src="images/logo.png" alt="Knowledgebase Site Logo"></img>
-            <div id="directory">
-                <table>
-                    <tbody>
-                        <tr><td><a href="#/"><p>Home</p></a></td></tr>
-                        <tr><td><a href="#about"><p>About</p></a></td></tr>
-                        <tr><td><a href="#login"><p>Login</p></a></td></tr>
-                    </tbody>
-                </table>
-            </div>
-        </div>
-    );
 };
 export default Directory;
